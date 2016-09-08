e1469b55951e4c20163eea3e94ff94d4ff122f4b
Javier <javier.rodriguez@alumnos.inf.utfsm.cl>
2016-09-08 19:24:00 -0300

    arreglos

--- kease.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-url = 'http://statweb.stanford.edu/~tibs/ElemStatLearn/datasets/prostate.data'
-df = pd.read_csv(url, sep='\t', header=0)
-df = df.drop('Unnamed: 0', axis=1)
-istrain_str = df['train']
-istrain = np.asarray([True if s == 'T' else False for s in istrain_str])
-istest = np.logical_not(istrain)
-df = df.drop('train', axis=1)
